12a76987f6415580878d383153263e628deb6de4
eskilq <eskilq@kth.se>
2019-10-06 23:45:50 +0200

    Added scene

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,32 @@
 mod basics;
 mod shapes;
+mod scene;
+mod raycast;
 
-use crate::basics::Vector3;
-use crate::shapes::Sphere;
+use crate::shapes::{Sphere, Light, Plane};
+use crate::scene::Scene;
+use crate::basics::Material;
 
 fn main() {
-    let s = Sphere::new(15.0, (30.0, 30.0, 10.0));
+    let mut scene: Scene = Scene::new((0.0, 0.0, 0.0));
 
-    for y in 0..60 {
-        for x in 0..60 {
-            let ray = Ray::new((x as f64, y as f64, 0.0), (0.0, 0.0, 1.0));
-            if s.ray_intersects(&ray).has_solution() {
-                print!("x");
-            } else {
-                print!(" ");
-            }
-        }
-        println!();
-    }
-}
+    scene.add(Box::new(Sphere::new(2.0, (0.0, -8.0, 13.0), Material::new((1.0, 0.0, 0.0), 0.3))));
+    scene.add(Box::new(Sphere::new(3.0, (-7.0, -7.0, 17.0), Material::new((0.2, 0.6, 0.9), 0.0))));
 
-struct Scene {
+    //Left and right walls
+    scene.add(Box::new(Plane::new((-10.0, 0.0, 0.0), (-10.0, 1.0, 0.0), (-10.0, 0.0, 1.0), Material::new((1.0, 1.0, 0.0), 0.2))));
+    scene.add(Box::new(Plane::new((10.0, 0.0, 0.0), (10.0, 0.0, 1.0), (10.0, 1.0, 0.0), Material::new((0.0, 1.0, 0.0), 0.2))));
 
-}
+    //Top and bottom walls
+    scene.add(Box::new(Plane::new((0.0, 10.0, 0.0), (1.0, 10.0, 0.0), (0.0, 10.0, 1.0), Material::new((0.0, 0.0, 1.0), 0.0))));
+    scene.add(Box::new(Plane::new((0.0, -10.0, 0.0), (0.0, -10.0, 1.0), (1.0, -10.0, 0.0), Material::new((1.0, 1.0, 1.0), 0.0))));
 
-struct Camera {
+    //Front and back walls
+    scene.add(Box::new(Plane::new((-1.0, -1.0, 20.0), (-1.0, 0.0, 20.0), (0.0, -1.0, 20.0), Material::new((1.0, 0.0, 1.0), 1.0))));
+    scene.add(Box::new(Plane::new((-1.0, -1.0, 0.0), (1.0, -1.0, 0.0), (-1.0, 1.0, 0.0), Material::new((0.0, 0.0, 0.0), 1.0))));
 
-}
+    scene.add_light(Light::new((-9.0, 6.0, 12.0), (1.0, 1.0, 1.0), 0.3));
+    scene.add_light(Light::new((9.0, 6.0, 12.0), (1.0, 1.0, 1.0), 0.3));
 
-struct Ray {
-    origin: Vector3,
-    direction: Vector3,
-}
-
-impl Ray {
-    fn new<T: Into<Vector3>>(origin: T, dir: T) -> Self {
-        Ray { origin: origin.into(), direction: dir.into().normalize() }
-    }
+    Scene::render(scene);
 }
